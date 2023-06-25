9d62c6f8ac3a35251dfdaaea2dbeffef82a3fe42
365support <365support@naver.com>
2023-06-25 18:51:26 +0900

    chore: pwa setting

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -30,7 +30,11 @@ export default class AppDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/icon.png"></link>
+          <meta name="theme-color" content="#fff" />
+        </Head>
         <body>
           <Main />
           <NextScript />
