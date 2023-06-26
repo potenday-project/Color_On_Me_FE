2593fac11f4aa2119e08cba7b90ea6c2df99f8c0
365support <365support@naver.com>
2023-06-26 19:59:24 +0900

    style: font setting

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -15,6 +15,13 @@ const GlobalStyle = () => (
       #__next {
         height: 100vh;
       }
+      @font-face {
+        font-family: "pretendard";
+        src: url("https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff")
+          format("woff");
+        font-weight: 400;
+        font-style: normal;
+      }
     `}
   />
 );
