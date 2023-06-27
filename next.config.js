bc843b80a216c9175e0f04b9658c71605d780b54
365support <365support@naver.com>
2023-06-27 23:53:47 +0900

    chore: @svgr/webpack 설치 세팅

--- next.config.js
+++ next.config.js
@@ -5,6 +5,13 @@ const nextConfig = {
   compiler: {
     removeConsole: process.env.NODE_ENV !== "development",
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
 };
 
 const withPWA = require("next-pwa")({
