0b6b6ae10d9af52a4a2d88f011ca46f6e553d7dc
365support <365support@naver.com>
2023-06-29 19:27:56 +0900

    feat: mypage BottomSheet 추가

--- src/domains/mypage/index.tsx
+++ src/domains/mypage/index.tsx
@@ -7,6 +7,8 @@ import Tag from "../shared/components/Tag";
 import Button from "../shared/components/Button";
 import Profile from "./components/Profile";
 import ColorBox from "./components/ColorBox";
+import { useIsShown } from "../shared/hooks/useIsShown";
+import BottomSheet from "../shared/components/BottomSheet";
 
 const colors = [
   "#75A0C8",
@@ -21,6 +23,8 @@ const colors = [
 const moodTags = ["#차분한", "#지적인", "#잔잔한"];
 
 const MypageView = () => {
+  const [isShown, onOpen, onClose] = useIsShown();
+
   const [isEdit, setIsEdit] = useState(false);
 
   const headerText = isEdit ? "프로필 수정" : "마이페이지";
@@ -28,83 +32,31 @@ const MypageView = () => {
   return (
     <DefaultLayout header={<Header>{headerText}</Header>}>
       <div css={mainContainer}>
-        <div
-          css={css`
-            margin-top: 3%;
-          `}
-        >
+        <div css={profileContainer}>
           <Profile />
         </div>
-        <div
-          css={css`
-            margin-top: 7%;
-          `}
-        >
+        <div css={titleContainer}>
           <TitleText>여름 쿨 뮤트</TitleText>
         </div>
-        <div
-          css={css`
-            display: flex;
-            justify-content: space-between;
-            margin-top: 4%;
-            height: 40%;
-          `}
-        >
+        <div css={colorBoxContainer}>
           {colors.map((color) => (
             <ColorBox key={color} color={color} />
           ))}
         </div>
 
-        <div
-          css={css`
-            display: flex;
-            justify-content: center;
-            gap: 10px;
-            margin-top: 10%;
-            @media (max-height: 800px) {
-              margin-top: 4%;
-            }
-          `}
-        >
+        <div css={tagContainer}>
           {moodTags.map((tag) => (
             <Tag key={tag}>{tag}</Tag>
           ))}
         </div>
-        <div
-          css={css`
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            flex-direction: column;
-            gap: 17px;
-
-            width: 100%;
-            margin-top: auto;
-            margin-bottom: 10%;
-            /* 아이폰 SE */
-            @media (max-height: 800px) {
-              gap: 8px;
-              margin-bottom: 5%;
-            }
-          `}
-        >
-          <Button variant="colored">퍼스널컬러 변경하기</Button>
-          <button
-            css={css`
-              width: 100%;
-              color: #8e9294;
-              font-size: 12px;
-              font-family: "Pretendard";
-
-              text-decoration: underline;
-              text-decoration-thickness: 1px;
-              text-underline-position: under;
-            `}
-          >
-            로그아웃
-          </button>
+        <div css={buttonContainer}>
+          <Button variant="colored" onClick={onOpen}>
+            퍼스널컬러 변경하기
+          </Button>
+          <button css={logoutButton}>로그아웃</button>
         </div>
       </div>
+      {isShown && <BottomSheet isOpen={isShown} close={onClose} />}
     </DefaultLayout>
   );
 };
@@ -118,6 +70,59 @@ const mainContainer = css`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+`;
+
+const profileContainer = css`
+  margin-top: 3%;
+`;
+
+const titleContainer = css`
+  margin-top: 7%;
+`;
+
+const colorBoxContainer = css`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 4%;
+  height: 40%;
+`;
+
+const tagContainer = css`
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+  margin-top: 10%;
+  @media (max-height: 800px) {
+    margin-top: 4%;
+  }
+`;
+
+const buttonContainer = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 17px;
+
+  width: 100%;
+  margin-top: auto;
+  margin-bottom: 10%;
+  /* 아이폰 SE */
+  @media (max-height: 800px) {
+    gap: 8px;
+    margin-bottom: 5%;
+  }
+`;
+
+const logoutButton = css`
+  width: 100%;
+  color: #8e9294;
+  font-size: 12px;
+  font-family: "Pretendard";
+
+  text-decoration: underline;
+  text-decoration-thickness: 1px;
+  text-underline-position: under;
 `;
 
 export default MypageView;
