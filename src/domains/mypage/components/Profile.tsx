e3dd893d8db9a99e0f92bf40d64bf5679d3fb699
365support <365support@naver.com>
2023-07-25 08:56:59 +0900

    feat: 프로필 닉네임 적용

--- src/domains/mypage/components/Profile.tsx
+++ src/domains/mypage/components/Profile.tsx
@@ -1,23 +1,29 @@
+import Image from "next/image";
 import { css } from "@emotion/react";
 import MainLogo from "@/domains/shared/components/MainLogo";
 
-const Profile = ({ data }: { data: { nickname: string; email: string } }) => {
+const Profile = ({
+  data,
+}: {
+  data: { name: string; email: string; profileImageUrl: string };
+}) => {
   return (
     <div css={container}>
-      <div css={logoContainer}>
-        <MainLogo />
-      </div>
-      {/* <Image
-        css={imageStyles}
-        src={
-          user?.profileImageUrl ? user?.profileImageUrl : "/images/profile.png"
-        }
-        alt="logo"
-        width={58}
-        height={58}
-      /> */}
+      {data.profileImageUrl ? (
+        <Image
+          css={imageStyles}
+          src={data?.profileImageUrl}
+          alt="logo"
+          width={58}
+          height={58}
+        />
+      ) : (
+        <div css={logoContainer}>
+          <MainLogo />
+        </div>
+      )}
       <div>
-        <div css={nameStyles}>{data?.nickname ? data?.nickname : "맹꽁이"}</div>
+        <div css={nameStyles}>{data?.name ? data?.name : "hi"}</div>
         <div css={emailStyles}>
           {data?.email ? data?.email : "365support@naver.com"}
         </div>
@@ -50,6 +56,10 @@ const emailStyles = css`
 const logoContainer = css`
   width: 58px;
   height: 58px;
+`;
+
+const imageStyles = css`
+  border-radius: 50%;
 `;
 
 export default Profile;
